d66d4251d3599ca8c9cdfc2c2ab948ce0ac39572
Davi Souza <dsl@ic.ufal.br>
2024-12-11 11:14:26 -0300

    :sparkles: feat: product routes tests

--- catalog_service/src/api/catalog.routes.ts
+++ catalog_service/src/api/catalog.routes.ts
@@ -1,12 +1,29 @@
 import { NextFunction, Request, Response, Router } from "express";
+import { CatalogService } from "../services/catalog.service";
+import { CatalogRepository } from "../repositories/catalog.repository";
+import { RequestValidator } from "../utils/requestValidator";
+import { CreateProductRequest } from "../dto/product.dto";
 
 const catalogRouter = Router();
 
+export const catalogService = new CatalogService(new CatalogRepository());
+
 catalogRouter.post(
-  "/product",
-  (req: Request, res: Response, next: NextFunction) => {
-    return res.status(201).json({});
+  "/products",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { errors, input } = await RequestValidator(
+        CreateProductRequest,
+        req.body
+      );
+      if (errors) return res.status(400).json(errors);
+      const data = await catalogService.createProduct(input);
+      return res.status(201).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
   }
 );
 
-export default catalogRouter; 
+export default catalogRouter;
--- catalog_service/src/services/__test__/catalog.service.test.ts
+++ catalog_service/src/services/__test__/catalog.service.test.ts
@@ -1,16 +1,9 @@
 import { ICatalogRepository } from "../../interfaces/catalogRepository.interface";
 import { Product } from "../../models/product.model";
 import { MockCatalogRepository } from "../../repositories/mockCatalog.repository";
+import { ProductFactory } from "../../utils/fixtures";
 import { CatalogService } from "../catalog.service";
 import { faker } from "@faker-js/faker";
-import { Factory } from "rosie";
-
-const productFactory = new Factory<Product>()
-  .attr("id", faker.number.int({ min: 1, max: 1000 }))
-  .attr("name", faker.commerce.productName())
-  .attr("description", faker.commerce.productDescription())
-  .attr("stock", faker.number.int({ min: 10, max: 100 }))
-  .attr("price", +faker.commerce.price());
 
 const mockProduct = (rest: any) => {
   return {
@@ -111,7 +104,7 @@ describe("catalogService", () => {
     test("should get products by offset and limit", async () => {
       const service = new CatalogService(repository);
       const randomLimit = faker.number.int({ min: 1, max: 50 });
-      const products = productFactory.buildList(randomLimit);
+      const products = ProductFactory.buildList(randomLimit);
       jest
         .spyOn(repository, "findAll")
         .mockImplementationOnce(() => Promise.resolve(products));
@@ -139,7 +132,7 @@ describe("catalogService", () => {
   describe("getProduct", () => {
     test("should get a product by id", async () => {
       const service = new CatalogService(repository);
-      const product = productFactory.build();
+      const product = ProductFactory.build();
       jest
         .spyOn(repository, "findOne")
         .mockImplementationOnce(() => Promise.resolve(product));
@@ -150,7 +143,7 @@ describe("catalogService", () => {
 
     test("should throw an error with product does not exist", async () => {
       const service = new CatalogService(repository);
-      const product = productFactory.build();
+      const product = ProductFactory.build();
 
       jest
         .spyOn(repository, "findOne")
@@ -167,7 +160,7 @@ describe("catalogService", () => {
   describe("deleteProduct", () => {
     test("should delete a product by id", async () => {
       const service = new CatalogService(repository);
-      const product = productFactory.build();
+      const product = ProductFactory.build();
       jest
         .spyOn(repository, "delete")
         .mockImplementationOnce(() => Promise.resolve({ id: product.id }));
@@ -180,7 +173,7 @@ describe("catalogService", () => {
 
     test("should throw an error with product does not exist", async () => {
       const service = new CatalogService(repository);
-      const product = productFactory.build();
+      const product = ProductFactory.build();
 
       jest
         .spyOn(repository, "delete")
